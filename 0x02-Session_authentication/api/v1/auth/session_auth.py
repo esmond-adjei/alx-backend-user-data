c47932d3b85b68dfc5b0bb7d8373cfa62461698b
esmond-adjei <xmongram@gmail.com>
2024-01-18 23:59:27 +0000

    modified some files - 2024-01-18 23:59:27

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,6 +21,8 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns user id based on session id
+        """
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
